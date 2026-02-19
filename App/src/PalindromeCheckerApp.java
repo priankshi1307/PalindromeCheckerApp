aa09b76f5357ea90849801b2d0d9ce98a149732f
priankshi <ps6312@srmist.edu.in>
2026-02-19 10:10:55 +0530

    priankshi Added UC2-HardCodePalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -19,12 +19,39 @@
  * The goal is to establish a clear startup flow.
  *
  * @auhor Priankshi
- * @version 1.0
+ * @version 2.0
  */
 
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
+    public static boolean isPalindrome(String text) {
+        // Preprocess: remove spaces, convert to lowercase
+        text = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0, right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
     public static void main(String[] args ){
         System.out.println("Welcome to Palindrome Checker App Management System");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Palindrome Checker App ===");
+        System.out.print("Enter a word, phrase, or number: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Yes '" + input + "' is a palindrome!");
+        } else {
+            System.out.println("No '" + input + "' is NOT a palindrome.");
+        }
+
+        scanner.close();
     }
 }
+
