12ec9b47544af05cc8dbcac2b7f25cbf56d66738
priankshi1307 <ps6312@srmist.edu.in>
2026-03-13 08:34:29 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -22,33 +22,3 @@
  * @version 1.0
  */
 
-import java.util.Scanner;
-public class PalindromeCheckerApp {
-    public static boolean isPalindrome(String text) {
-        text = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int left = 0, right = text.length() - 1;
-        while (left < right) {
-            if (text.charAt(left) != text.charAt(right)) {
-            return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-    public static void main(String[] args ){
-        System.out.println("Welcome to Palindrome Checker App Management System");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("=== Palindrome Checker App ===");
-        System.out.print("Enter a word, phrase, or number: ");
-        String input = scanner.nextLine();
-        if (isPalindrome(input)) {
-            System.out.println("yes" + input + "' is a palindrome!");
-        } else {
-            System.out.println("no" + input + "' is NOT a palindrome.");
-        }
-        scanner.close();
-    }
-
-}
-
